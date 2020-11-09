c492347487b39c066ff2a91a9f542a13e51d490c
SiRaZz <darius.madeikis@gmail.com>
2020-11-09 20:33:09 +0200

    Leasing App:Initial commit
    Initial commit

---
+++ src/main/java/DanskeBank/carLeasingApp/CarLeasingAppApplication.java
@@ -0,0 +1,13 @@
+package DanskeBank.carLeasingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLeasingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLeasingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DanskeBank/carLeasingApp/CarLeasingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package DanskeBank.carLeasingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLeasingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
